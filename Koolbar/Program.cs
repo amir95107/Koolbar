7649ba75d6c0a36996b08fcc901c7b08f205037c
am.janmohammadi <am.janmohammadi@JANMOHAMMADI--A>
2024-03-29 13:12:05 +0330

    publish for linux

--- Koolbar/Program.cs
+++ Koolbar/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 var configuration = builder.Configuration;
+
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
