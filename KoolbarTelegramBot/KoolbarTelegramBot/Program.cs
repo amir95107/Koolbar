7a5c97f76d8047db37cad4ec9721d16277ea6c3c
am.janmohammadi <am.janmohammadi@JANMOHAMMADI--A>
2024-04-15 01:26:27 +0330

    a

--- KoolbarTelegramBot/KoolbarTelegramBot/Program.cs
+++ KoolbarTelegramBot/KoolbarTelegramBot/Program.cs
@@ -1,7 +1,7 @@
 ﻿using KoolbarTelegramBot;
 using Telegram.Bot;
-const string token = "7001628889:AAGkzn7E2Uw2vs9aFo3GCG2V-kuheFdz4BU";
-//const string token = "6987060819:AAF4G3gaSyRDWf_NuT21tCJBU3ukmQ9rxxw";
+//const string token = "7001628889:AAGkzn7E2Uw2vs9aFo3GCG2V-kuheFdz4BU";
+const string token = "6987060819:AAF4G3gaSyRDWf_NuT21tCJBU3ukmQ9rxxw";
 //var _secret = "1603010200010001fc030386e24c3add";
 //var proxy = new WebProxy("142.132.201.180", 443, Credentials: );
 //ICredentials credentials = new NetworkCredential(cred);
